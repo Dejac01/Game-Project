3bbf4f48234bc2d6e92f10ce0863228f5a4bd475
deja.craft <dejacraft28@gmail.com>
2023-02-21 10:36:50 -0500

    Game Project

---
+++ app.js
@@ -0,0 +1,8 @@
+//Card matching game
+//Instructions: Two player matching card game
+// During round one there will be 4 seconds to view the back of each card for memorization
+//Each player will take turns clicking on two cards each turn to find the matching sets
+//Every round will increase in difficulty with less time to view cards, less time to match cards and more cards will be added each round
+//The player with the most matched sets in the alloted time wins. 
+
+
