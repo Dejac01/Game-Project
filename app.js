3f3b52b6ba5f1f2750b7049af1294072e32d1e69
deja.craft <dejacraft28@gmail.com>
2023-02-27 13:04:06 -0500

    game

--- app.js
+++ app.js
@@ -5,8 +5,9 @@
 //Second round will increase in difficulty with less time to view cards, less time to match cards and more cards will be added
 //The player with the most matched sets in the alloted time wins. 
 
+window.alert("Instructions: Two player matching card game. During round one there will be 3 seconds to view the back of each card for memorization. Each player will take turns clicking on two cards each turn to find the matching set.The player with the most matched sets in the alloted time wins. ");
 document.addEventListener('DOMContentLoaded', () => {
-    window.alert("Instructions: Two player matching card game. During round one there will be 3 seconds to view the back of each card for memorization. Each player will take turns clicking on two cards each turn to find the matching set.The player with the most matched sets in the alloted time wins. ");
+  
 // Card sets Array
 const cardSet =[
     {
